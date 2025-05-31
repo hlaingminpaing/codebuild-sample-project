96acdd3ce7f3b8ef1e916491479d46a8a4810733
Hlaing Min Paing <hlaingminpaing.ygn@gmail.com>
2025-05-31 14:47:25 +0700

    update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ exports.handler = async (event) => {
   console.log("Received event:", JSON.stringify(event, null, 2));
   return {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
+    body: JSON.stringify("Hello from Lambda-CodeDeploy!"),
   };
 };
